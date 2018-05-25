30d0c7d1a0911a3189bb5034dfbd1676c6f834de
Chris Nowicki <chris.nowicki@citrix.com>
2018-05-25 11:37:45 -0500

    Added server and client recipes

---
+++ recipes/client.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: plex-hackday
+# Recipe:: client
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
---
+++ recipes/server.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: plex-hackday
+# Recipe:: server
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+apt_package 'package_name' do
+  action :install
+end
+
+
