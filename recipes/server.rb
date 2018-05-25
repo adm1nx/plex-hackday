113099168a634045e226a5859f6e4cfe21e97b6a
Chris Nowicki <chris.nowicki@citrix.com>
2018-05-25 12:54:14 -0500

    installed server package

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,16 +4,17 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 # TODO: This is dropping "xenial" in the deb line. we might need to remove it.
-apt_repository "plex" do
-  uri "https://downloads.plex.tv/repo/deb"
-  components ["./public", "main"]
-  key "https://downloads.plex.tv/plex-keys/PlexSign.key"
-  action :add
+remote_file "/opt/plexmediaserver.deb" do
+  source node["plex-hackday"]["deb"]["package"]
+  owner 'root'
+  group 'root'
+  mode '0755'
+  checksum node["plex-hackday"]["deb"]["checksum"]
+  action :create
 end
 
-apt_update "plex" do
-#   ignore_failure "true"
-  action :update
+apt_package "/opt/plexmediaserver.deb" do
+  action :install
 end
 
 # TODO: figure out the correct package name and install Plex server
