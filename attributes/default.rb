a5808793d18241c9a20f4725830abef939c1a96e
Chris Nowicki <chris.nowicki@citrix.com>
2018-05-25 13:39:53 -0500

    Changed media library path

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default["plex-hackday"]["deb"]["package"] = "https://downloads.plex.tv/plex-media-server/1.13.0.5023-31d3c0c65/plexmediaserver_1.13.0.5023-31d3c0c65_amd64.deb"
 default["plex-hackday"]["deb"]["checksum"] = "b6c8184abba84ac18b5db2d7b80d2ef4ca77cc616f99c7ffb999ae4af42f868e"
 
-default["plex-hackday"]["library_path"] = "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/"
+default["plex-hackday"]["library_path"] = "/var/lib/plexmediaserver/Movies"
